f8d87348007d695ff6f4698bd46b6b33f10bd1d4
BilalMP <peeroo270@gmail.com>
2024-11-24 18:14:39 +0400

    Closure

---
+++ src/pages/Closure.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import State from '../components/Closure/State'
+import CustomHook from '../components/Closure/CustomHook'
+import Alert from '../components/Closure/Alert'
+
+const Closure = () => {
+    return (
+        <div>
+            <State />
+            <CustomHook />
+            <Alert />
+        </div>
+    )
+}
+
+export default Closure
