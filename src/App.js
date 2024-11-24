d07bdb955d687075f231833e40d3b153223d07dd
BilalMP <peeroo270@gmail.com>
2024-11-24 18:15:01 +0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { Routes, Route } from 'react-router-dom';
+import AdvancedJsx from './pages/AdvancedJsx';
+import FormPage from './pages/FormPage';
+import Destructuring from './pages/Destructuring';
+import DOMElement from './pages/DOMElement';
+import Closure from './pages/Closure';
+import ConditionalRendering from './pages/ConditionalRendering';
+import ArrayImmutability from './pages/ArrayImmutability';
+import ObjectImmutability from './pages/ObjectImmutability';
+import UseEffect from './pages/UseEffect';
+import UseCallback from './pages/UseCallback';
+import APIFetching from './pages/APIFetching';
+import ContextProvider from './pages/ContextProvider';
+
 const App = () => {
   return (
-    <div className="">
-     
-    </div>
+    <Routes>
+      <Route path="/advancedjsx" element={<AdvancedJsx />} />
+      <Route path="/form" element={<FormPage />} />
+      <Route path="/destructuring" element={<Destructuring />} />
+      <Route path="/domelement" element={<DOMElement />} />
+      <Route path="/closure" element={<Closure />} />
+      <Route path="/conditionalrendering" element={<ConditionalRendering />} />
+      <Route path="/arrayimmutability" element={<ArrayImmutability />} />
+      <Route path="/objectimmutability" element={<ObjectImmutability />} />
+      <Route path="/useeffect" element={<UseEffect />} />
+      <Route path="/usecallback" element={<UseCallback />} />
+      <Route path="/apifetching" element={<APIFetching />} />
+      <Route path="/contextprovider" element={<ContextProvider />} />
+    </Routes>
   );
 }
 
